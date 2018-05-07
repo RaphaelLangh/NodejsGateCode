f913c920b2bf1201b6c981ec64858830e6c70021
RaphaelLangh <r.langhendries@laposte.net>
2018-05-07 20:08:38 +0200

    jQuery

--- index.js
+++ index.js
@@ -4,75 +4,89 @@ var app = express();
 var andrFunc = require('./lib/AndroidCom/index.js');
 var webAppFuncs = require('./lib/WebAppCom/index.js');
 
-// road for andoid phone 
+// road for andoid phone
 
-// retourn the gate connection information 
+// retourn the gate connection information
 app.get('/beginMission', function(req, res) {
-    console.log("hitten beginMiss");
-    andrFunc.beginMission(req.query.imei, function(err, gateData) {
-        if (err) { res.sendStatus(404); } else {
-            res.send(gateData);
-        }
-    });
+  console.log("hitten beginMiss");
+  andrFunc.beginMission(req.query.imei, function(err, gateData) {
+    if (err) {
+      res.sendStatus(404);
+    } else {
+      res.send(gateData);
+    }
+  });
 });
 app.get('/openGate', function(req, res) {
-    andrFunc.openGate(req.query.imei, function(err, passwords) {
-        if (err) { res.sendStatus(404); } else {
-            res.send(passwords);
-        }
-    });
+  andrFunc.openGate(req.query.imei, function(err, passwords) {
+    if (err) {
+      res.sendStatus(404);
+    } else {
+      res.send(passwords);
+    }
+  });
 });
 app.get('/closeGate', function(req, res) {
-    andrFunc.closeGate(req.query.imei, req.query.id_gate, req.query.password, req.query.nPassword, function(err, message) {
-        if (err) { res.sendStatus(404); } else {
-            res.send(message);
-        }
-    });
+  andrFunc.closeGate(req.query.imei, req.query.id_gate, req.query.password, req.query.nPassword, function(err, message) {
+    if (err) {
+      res.sendStatus(404);
+    } else {
+      res.send(message);
+    }
+  });
 });
 
 
-// old road 
+// old road
 app.get('/gatePassword', function(req, res) {
-    andrFunc.getGatePassword(req.query.id_gate, req.query.imei, function(err, password) {
-        if (err) { res.sendStatus(403); } else {
-            res.send([password]);
-        }
-    });
+  andrFunc.getGatePassword(req.query.id_gate, req.query.imei, function(err, password) {
+    if (err) {
+      res.sendStatus(403);
+    } else {
+      res.send([password]);
+    }
+  });
 });
 app.get('/changePassword', function(req, res) {
-    andrFunc.changingGatePassword(req.query.id_gate, req.query.imei, function(err, newPassword) {
-        if (err) { res.sendStatus(403); } else {
-            res.send([newPassword]);
-        }
-    });
+  andrFunc.changingGatePassword(req.query.id_gate, req.query.imei, function(err, newPassword) {
+    if (err) {
+      res.sendStatus(403);
+    } else {
+      res.send([newPassword]);
+    }
+  });
 });
 
 // road for webapp
 app.get('/agents', function(req, res) { // we want the list of agents curently employed
-    webAppFuncs.ListAgent(function(err, result) {
-        if (err) { res.sendStatus(403); } else {
-            res.send(result);
-        }
-    });
+  webAppFuncs.ListAgent(function(err, result) {
+    if (err) {
+      res.sendStatus(403);
+    } else {
+      res.send(result);
+    }
+  });
 });
 
-app.get('/agentAuth', function(req, res) { // we want the gate that an agent can open 
+app.get('/agentAuth', function(req, res) { // we want the gate that an agent can open
 
-    webAppFuncs.agentAuth(req.query.id_agent, function(err, gateList) {
-        if (err) { res.sendStatus(403); } else {
-            res.send(gateList);
-        }
-    });
+  webAppFuncs.agentAuth(req.query.id_agent, function(err, gateList) {
+    if (err) {
+      res.sendStatus(403);
+    } else {
+      res.send(gateList);
+    }
+  });
 });
 
-app.get('/givAgentAuth', function(req, res) { // alow agent to access to a given gate   
-    webAppFuncs.givAgentAuth(req.query.id_agent, req.query.id_gate);
-    res.sendStatus(200);
+app.get('/givAgentAuth', function(req, res) { // alow agent to access to a given gate
+  webAppFuncs.givAgentAuth(req.query.id_agent, req.query.id_gate);
+  res.sendStatus(200);
 });
 
-app.get('/delAgentAuth', function(req, res) { // don't anymore alow agent to access to a given gate   
-    webAppFuncs.delAgentAuth(req.query.id_agent, req.query.id_gate);
-    res.sendStatus(200);
+app.get('/delAgentAuth', function(req, res) { // don't anymore alow agent to access to a given gate
+  webAppFuncs.delAgentAuth(req.query.id_agent, req.query.id_gate);
+  res.sendStatus(200);
 });
 
 
